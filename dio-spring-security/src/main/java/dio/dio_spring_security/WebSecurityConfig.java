be1f8204b7e8d52b45c39f9bd2653a0cd665f796
Dvd-coder-art <daviddavi1221@gmail.com>
2025-02-20 14:36:08 -0300

    Upando arquivos do módulo de API REST DIO
    Signed-off-by: Dvd-coder-art <daviddavi1221@gmail.com>

---
+++ dio-spring-security/src/main/java/dio/dio_spring_security/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package dio.dio_spring_security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers("/managers").hasRole("MANAGERS")
+                        .requestMatchers("/users").hasAnyRole("USERS", "MANAGERS")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login.defaultSuccessUrl("/", true)) // Após login, redireciona para "/"
+                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/")); // Configuração de logout
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("user123"))
+                .roles("USERS")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("master123"))
+                .roles("MANAGERS")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
